02b12cfc63bcb4275f7c77b4007dd0c3c851ebe9
LukasKlenner <klenner-lukas@t-online.de>
2024-12-07 19:04:09 +0100

    Day5

--- src/main/java/aoc/year24/day5/Day5.java
+++ src/main/java/aoc/year24/day5/Day5.java
@@ -2,13 +2,52 @@ package aoc.year24.day5;
 
 import aoc.JoinedDay;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day5 implements JoinedDay {
 
     @Override
     public Object run(Stream<String> stream, boolean part1) {
-        return null;
-    }
 
+        Iterator<String> input = stream.iterator();
+
+        Map<Integer, Set<Integer>> greaterMap = new HashMap<>();
+
+        String line;
+        while (!(line = input.next()).isEmpty()) {
+            String[] numbers = line.split("\\|");
+            greaterMap.computeIfAbsent(Integer.parseInt(numbers[1]), k -> new HashSet<>()).add(Integer.parseInt(numbers[0]));
+        }
+
+        int result = 0;
+
+        while (input.hasNext()) {
+
+            line = input.next();
+
+            List<Integer> page = Arrays.stream(line.split(",")).map(Integer::parseInt).collect(Collectors.toList());
+
+            boolean changed = false;
+            for (int i = 0; i < page.size(); i++) {
+                for (int j = 0; j < i; j++) {
+                    if (greaterMap.containsKey(page.get(j)) && greaterMap.get(page.get(j)).contains(page.get(i))) {
+                        page.add(j, page.remove(i));
+                        changed = true;
+                    }
+                }
+            }
+
+            if ((part1 && !changed) || (!part1 && changed)) result += page.get(page.size() / 2);
+        }
+
+        return result;
+    }
 }
