5d48c3ceed2b3bf8dbdfdd6d3b06ed3d09eeba2b
LukasKlenner <klenner-lukas@t-online.de>
2025-12-07 11:07:23 +0100

    day3 & 7

--- src/main/kotlin/aoc/year25/Day03.kt
+++ src/main/kotlin/aoc/year25/Day03.kt
@@ -5,6 +5,23 @@ import aoc.KtJoinedDay
 class Day03 : KtJoinedDay {
 
     override fun run(input: List<String>, part1: Boolean): Long {
-        TODO("Not yet implemented")
+        val batteryCount = if (part1) 2 else 12
+        var result = 0L
+
+        input.forEach { line ->
+
+            val voltage = StringBuilder()
+            var startIndex = 0
+
+            for (i in batteryCount downTo 1) {
+                val max = line.substring(startIndex, line.length - i + 1).maxOf { it.digitToInt() }
+                voltage.append(max)
+                startIndex = line.indexOf(max.digitToChar(), startIndex) + 1
+            }
+
+            result += voltage.toString().toLong()
+        }
+
+        return result
     }
 }
--- src/main/kotlin/aoc/year25/Day07.kt
+++ src/main/kotlin/aoc/year25/Day07.kt
@@ -5,6 +5,63 @@ import aoc.KtJoinedDay
 class Day07 : KtJoinedDay {
 
     override fun run(input: List<String>, part1: Boolean): Long {
-        TODO("Not yet implemented")
+
+        State.splitCount = 0L
+
+        val startState = State(LongArray(input[0].length) { i -> if (input[0][i] == 'S') 1 else 0 })
+        val lines = input.drop(1).map { Line.fromString(it) }
+
+        val finalState = lines.fold(startState) { state, line -> state.apply(line) }
+
+        return if (part1) State.splitCount else finalState.state.sum()
     }
+
+    private class Line(val content: BooleanArray) {
+
+        fun isSplitter(index: Int): Boolean {
+            return content[index]
+        }
+
+        companion object {
+            fun fromString(s: String): Line {
+                val content = BooleanArray(s.length) { s[it] == '^' }
+                return Line(content)
+            }
+        }
+
+    }
+
+    private class State(val state: LongArray) {
+
+        fun apply(line: Line): State {
+            val newState = State(LongArray(state.size) { 0 })
+
+            for (i in state.indices) {
+                if (isRay(i)) {
+                    if (line.isSplitter(i)) {
+                        newState.addRay(i - 1, state[i])
+                        newState.addRay(i + 1, state[i])
+                        splitCount++
+                    } else {
+                        newState.addRay(i, state[i])
+                    }
+                }
+            }
+
+            return newState
+        }
+
+        fun addRay(index: Int, count: Long) {
+            state[index] += count
+        }
+
+        fun isRay(index: Int): Boolean {
+            return state[index] > 0
+        }
+
+        companion object {
+            var splitCount = 0L
+        }
+    }
+
 }
